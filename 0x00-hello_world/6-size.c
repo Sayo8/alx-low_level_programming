6b965f53481675f0be5a402bbf415a06f9671d73
Oluwasayo Adetutu <oluwasayoadetutu5@gmail.com>
2022-12-08 13:47:10 -0800

    6 size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+/**
+ * main - print out size of data types in C
+ * Return: (0)
+*/
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(int));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
