e7734fb3d1ea6e059b3bc621946e3aa071ab85b0
Oluwasayo Adetutu <oluwasayoadetutu5@gmail.com>
2022-12-08 15:00:28 -0800

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-	dprintf(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
+	dprintf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
